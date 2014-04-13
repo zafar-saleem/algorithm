f5e5fb610fbb6f737a48c68219b577a5f851fb78
zafar-saleem <zafarsaleem3@gmail.com>
2014-04-13 16:30:38 +0200

    Added use strict and some minor fixes

--- js/algorithm.js
+++ js/algorithm.js
@@ -1,4 +1,5 @@
 define(function() {
+	"use strict";
 
 	var Algorithm = {
 
@@ -16,33 +17,33 @@ define(function() {
     getAllEmployeeIds: function () {
     	var self = this;
 
-			if (self.flag) {
-				$.getJSON('data/employees.json', function (data) {
-	    		self.separateTeamIds(data, self.hkiTeam, self.nyTeam);
-	    		self.invitees(self.hkiTeam, self.nyTeam, self.result);
-		    		
-					for (var n in self.result) {
-					  self.output += '<li>' + n + '</li>';
-					}
-					
-					$(selectees).html(self.output);
-						self.flag = false;
-		    	});
-			}
+			if (!self.flag) return;
+
+			$.getJSON('data/employees.json', function (data) {
+    		self.separateTeamIds(data, self.hkiTeam, self.nyTeam);
+    		self.invitees(self.hkiTeam, self.nyTeam, self.result);
+	    		
+				for (var n in self.result) {
+				  self.output += '<li>' + n + '</li>';
+				}
+				
+				$(selectees).html(self.output);
+				self.flag = false;
+	   	});
     },
 
     separateTeamIds: function (data, hkiTeam, nyTeam) {
 			for (var i = 0; i < data.length; i++) {
-		    var el 		= data[i].split(' ');
-		    hkiTeam[i]  = el[0];
-		    nyTeam[i]   = el[1];
+		    var el = data[i].split(' ');
+		    hkiTeam[i] = el[0];
+		    nyTeam[i] = el[1];
 			}
     },
 
     invitees: function (hkiTeam, nyTeam, result) {
     	for (var k = 0; k < hkiTeam.length; k++) {
 		    var nOfFirst = this.countElements(hkiTeam, hkiTeam[k]),
-		    	nOfSecond  = this.countElements(nyTeam, nyTeam[k]);
+		    		nOfSecond = this.countElements(nyTeam, nyTeam[k]);
 		    
 		    if (nOfFirst > nOfSecond) {
 		      result[hkiTeam[k]] = 0;
